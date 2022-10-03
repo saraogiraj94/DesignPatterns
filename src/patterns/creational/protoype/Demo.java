4bfa64dc2234584ab3ce2c2e747c508525a44940
saraogiraj94 <saraogi.raj40@gmail.com>
2022-10-03 21:38:09 +0530

    feat(prototype): added bookshops and books demo

--- src/patterns/creational/protoype/Demo.java
+++ src/patterns/creational/protoype/Demo.java
@@ -1,7 +1,16 @@
 package patterns.creational.protoype;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        BookShop abcBookShop = new BookShop();
+        abcBookShop.setShopName("ABC Bookshop");
+        abcBookShop.loadData();
+        System.out.println(abcBookShop);
+
+        BookShop defBookshop = (BookShop) abcBookShop.clone();
+        System.out.println(defBookshop);
+        defBookshop.getBooks().remove(1);
+        System.out.println(abcBookShop);
 
     }
 }
